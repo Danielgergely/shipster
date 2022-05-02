a5d40aeee2ebe1b051a4efa591c6fc377de64793
Timo Grünenfelder <timo.gruenenfelder@students.fhnw.ch>
2022-05-02 21:02:03 +0200

    Attributes, Getters, Setters for Article Class

--- src/main/java/ch/shipster/data/domain/Article.java
+++ src/main/java/ch/shipster/data/domain/Article.java
@@ -12,6 +12,54 @@ public class Article {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /// Attributes
+    private String name;
+    private float price;
+    private float palletSpace;
+    private float maxStack;
+
     /// Constructor
     public Article(){}
+
+    /// Getter & Setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public float getPalletSpace() {
+        return palletSpace;
+    }
+
+    public void setPalletSpace(float palletSpace) {
+        this.palletSpace = palletSpace;
+    }
+
+    public float getMaxStack() {
+        return maxStack;
+    }
+
+    public void setMaxStack(float maxStack) {
+        this.maxStack = maxStack;
+    }
 }
