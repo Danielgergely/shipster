91d6f19fcde6de9f9a6f268990f80e9314d122bc
Timo Grünenfelder <timo.gruenenfelder@students.fhnw.ch>
2022-05-02 21:16:11 +0200

    Attributes, Getters, Setters for Order Class

--- src/main/java/ch/shipster/data/domain/Order.java
+++ src/main/java/ch/shipster/data/domain/Order.java
@@ -2,6 +2,7 @@ package ch.shipster.data.domain;
 
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name="order")
@@ -12,6 +13,93 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /// Attributes
+    private long userId;
+    private OrderStatus orderStatus;
+
+    private Date lastUpdateDate;
+    private Date basketDate;
+    private Date orderDate;
+    private Date shippingDate;
+    private Date deliveryDate;
+    private Date cancellationDate;
+
     /// Constructor
     public Order(){}
+
+    /// Getter & Setter
+
+    //TODO getUser (Instead of getUserId)
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public Date getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+
+    public void setLastUpdateDate(Date lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate;
+    }
+
+    public Date getBasketDate() {
+        return basketDate;
+    }
+
+    public void setBasketDate(Date basketDate) {
+        this.basketDate = basketDate;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Date getShippingDate() {
+        return shippingDate;
+    }
+
+    public void setShippingDate(Date shippingDate) {
+        this.shippingDate = shippingDate;
+    }
+
+    public Date getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(Date deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public Date getCancellationDate() {
+        return cancellationDate;
+    }
+
+    public void setCancellationDate(Date cancellationDate) {
+        this.cancellationDate = cancellationDate;
+    }
 }
